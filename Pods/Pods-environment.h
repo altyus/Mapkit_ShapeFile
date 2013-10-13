e00873ddcba8330aa88779cf09fad2fbd091b243
altyus <alctyus@gmail.com>
2013-10-13 12:25:43 -0400

    added shapelib CocoaPod

---
+++ Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// shapelib
+#define COCOAPODS_POD_AVAILABLE_shapelib
+#define COCOAPODS_VERSION_MAJOR_shapelib 1
+#define COCOAPODS_VERSION_MINOR_shapelib 3
+#define COCOAPODS_VERSION_PATCH_shapelib 0
+
